217f61b343dd4956f46eb669373e330299dfd21a
unknown <>
2017-02-25 22:43:10 +0800

    提交static 和 vender

---
+++ public/static/assets/js/jijuxie.admin.js
@@ -0,0 +1,28 @@
+/**
+ * Created by 12156 on 2017/2/23.
+ */
+$(document).ready(function () {
+    //全屏方法(切换)
+    function screenBigOpen() {
+
+        if ($.AMUI.fullscreen.enabled) {
+            // putmsg.animal('本浏览器支持全屏');
+            $.AMUI.fullscreen.toggle();
+        } else {
+            putmsg.animal('本浏览器不支持全屏');
+        }
+    }
+
+    $('.tpl-big-open-button').click(function () {
+        screenBigOpen();
+    });
+    //菜单点击 iframe切换src
+    function changeIframeSrc(url) {
+        $('#main_iframe').attr('src', url);
+    }
+    $(".jijuxie_iframe_menu").click(function () {
+        alert($(this).attr('data-change-url'));
+        changeIframeSrc($(this).attr('data-change-url'));
+
+    });
+});
